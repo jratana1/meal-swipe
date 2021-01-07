12fc6630379bbac6a52d2d08af69bf4e51fc6b54
John Ratana <jratana1@gmail.com>
2021-01-07 18:13:29 -0500

    added slug to restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,14 +2,12 @@ class Restaurant < ApplicationRecord
     has_many :photos
     has_many :likes, dependent: :destroy
     has_many :liking_users, through: :likes, source: :user
-    
-    # extend Slugifiable::ClassMethods
-    # include Slugifiable::InstanceMethods
 
     validates :address, presence: true
     validates :zipcode, presence: true
     validates :name, presence: true, uniqueness: { scope: :address, :zipcode
     message: "already exists" }
 
-
+    extend FriendlyId
+    friendly_id :name, :use => [:slugged]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_220453) do
+ActiveRecord::Schema.define(version: 2021_01_07_231236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 2021_01_07_220453) do
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_restaurants_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
