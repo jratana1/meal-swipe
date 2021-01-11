d8eca9601ea5f7a03a0582da876eca51af86f4f5
John Ratana <jratana1@gmail.com>
2021-01-11 02:15:32 -0500

    create and destroy likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,25 @@
 class LikesController < ApplicationController
     def create
+        like = Like.create(like_params)
+        flash[:notice] = "You now like #{like.restaurant.name}."
+        redirect_to restaurant_path(like.restaurant)
     end
 
     def destroy
+      like = current_user.likes.find_by(restaurant_id: params[:like][:restaurant_id])
+
+      if like != nil     
+          flash[:notice] = "You have unliked #{like.restaurant.name}."
+          like.destroy
+      else
+          flash[:notice] = "You already don't like that restaurant."      
+      end
+      redirect_to restaurants_path
+    end
+
+    private
+
+    def like_params
+        params.require(:like).permit(:restaurant_id, :user_id)
     end
 end
