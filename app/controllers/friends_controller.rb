b9a2d5972b40a822bf46bde19d696381e4b76354
John Ratana <jratana1@gmail.com>
2021-01-11 21:32:01 -0500

    Can unfriend

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,24 +1,26 @@
 class FriendsController < ApplicationController
     def index
-        #pending friend
-        #requested friends
+        #list pending friend
+        #list requested friends
     end
 
-    def create
-        #friend
-        
+    def create 
         friend = User.find_by(id: params[:friend][:id])
         current_user.friend_request(friend)
         flash[:alert] = "Friend request made to #{friend.name}."
-        byebug
         redirect_to users_path
     end
 
     def update
-        #add/reject
+        #add/reject pending
     end
 
     def destroy
         #unfriend
+        byebug
+        friend = User.find_by(id: params[:id])
+        current_user.remove_friend(friend)
+        flash[:alert] = "You have unfriended #{friend.name}."
+        redirect_to users_path
     end
 end
