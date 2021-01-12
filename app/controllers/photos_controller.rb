b1a8faab37170ff2c8e0095395041c00e85e6f96
John Ratana <jratana1@gmail.com>
2021-01-11 23:30:33 -0500

    can delete phot

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,10 @@
 class PhotosController < ApplicationController
     def index
+        @photos = current_user.photos
     end
 
     def show
+        @photo = Photo.find_by_id(params[:id])
     end
 
     def new
@@ -12,5 +14,11 @@ class PhotosController < ApplicationController
     end
 
     def destroy
+        #nned to check photo belongs to current user
+        #need to hide delete button on other users pages.
+        user = User.find_by_id(params[:user_id])
+        photo = Photo.find_by_id(params[:id])
+        photo.destroy
+        redirect_to user_photos_path(user)
     end
 end
