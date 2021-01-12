fd780e966a7a2122c48ad447a50ae47f60a9439b
John Ratana <jratana1@gmail.com>
2021-01-12 11:18:42 -0500

    cut more routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :photos
   resources :users do
     resources :restaurants
-    resources :photos
+    resources :photos, only: [:show, :index, :new, :create, :destroy]
   
   end
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
