9af9cac8cadde84955d9cee8d9d547c3a729b57c
John Ratana <jratana1@gmail.com>
2021-01-13 11:17:24 -0500

    nested form for photo/restaurant

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,24 +4,41 @@ class PhotosController < ApplicationController
     end
 
     def show
+        #add back button to view
         @photo = Photo.find_by_id(params[:id])
     end
 
     def new
+        @photo = Photo.new
+        @photo.build_restaurant
     end
 
     def create
+        byebug
+        @photo = Photo.new(photo_params)
+
+        if @photo.save
+            flash[:alert] = "You have added a photo."
+            redirect_to user_photos_path(current_user)
+        else
+            render 'new'
+        end
     end
 
     def destroy
-        user = User.find_by_id(params[:user_id])
         photo = Photo.find_by_id(params[:id])
-        if photo.user_id == user.id
-        photo.destroy
-        flash[:alert] = "Photo has been deleted." 
+
+        if photo.user_id == current_user.id
+            photo.destroy
+            flash[:alert] = "Photo has been deleted." 
         else
-        flash[:alert] = "That photo doesn't belong to you." 
+            flash[:alert] = "That photo doesn't belong to you." 
         end
-        redirect_to user_photos_path(user)
+            redirect_to user_photos_path(current_user)
+    end
+
+    private
+    def photo_params
+        params.require(:photo).permit(:url, :restaurant_id)
     end
 end
