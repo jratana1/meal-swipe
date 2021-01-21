e553aa4f12f734a85de7e2888185114e9bedbbac
John Ratana <jratana1@gmail.com>
2021-01-21 16:00:16 -0500

    fixing style sheets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+// import 'core-js/stable'
+// import 'regenerator-runtime/runtime'
 import 'bootstrap';
-import './stylesheets/application'
+import '../stylesheets/application'
 
 require("@rails/ujs").start()
 require("turbolinks").start()
